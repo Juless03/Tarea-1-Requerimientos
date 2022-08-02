73f03770f6d045e80dae36e4f631b140f03c74f2
Juleisy Porras <80361927+Juless03@users.noreply.github.com>
2022-08-02 14:33:59 -0600

    Delete cursorProcedures.sql

--- cursorProcedures.sql
+++
@@ -1,8 +0,0 @@
--- Retorna los emails de un usuario
-DELIMITER //
-DROP PROCEDURE IF EXISTS getUserEmails; //
-CREATE PROCEDURE getUserEmails(IN pPersonID INT) 
-BEGIN
-SELECT mail FROM email
-WHERE personRef = pPersonID;
-END; //
